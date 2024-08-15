0b5ef94a310116a53750166535aed431789350ea
Flavia <flavia2006martinez@gmail.com>
2024-08-15 10:00:37 -0400

    updated wrapper - added defaults to parser

--- ralf.py
+++ ralf.py
@@ -17,13 +17,13 @@ import main_routing
 
 # Define the argument parser
 parser = argparse.ArgumentParser(description='Circuit placement program')
-parser.add_argument('placement_type', choices=['rl', 'rps'], help='Type of placement (rl for reinforcement learning, rps for simulated annealing)')
 parser.add_argument('circuit_name', help='Name of the circuit')
+parser.add_argument('-p', '--placement_type', choices=['rl', 'rps'], help='Type of placement (rl for reinforcement learning, rps for simulated annealing)', default = 'rl')
 
-parser.add_argument('PLAN_WIRES',  type=bool, help='activates the planner')
-parser.add_argument('N_PLANNING_ITERATIONS', type=int,help='defines the number of planning iterations')
-parser.add_argument('GCELL_LENGTH', help=' defines the width and height of a grid cell (150 is recommended)')
-parser.add_argument('LAYERS', help='for defining the usable layers')
+parser.add_argument('-w', '--PLAN_WIRES',  type=bool, help='activates the planner', default = main_routing.DEFAULT_PLAN_WIRES)
+parser.add_argument('-n','--N_PLANNING_ITERATIONS', type=int,help='defines the number of planning iterations', default = main_routing.DEFAULT_N_PLANNING_ITERATIONS)
+parser.add_argument('-g','--GCELL_LENGTH', help=' defines the width and height of a grid cell (150 is recommended)', default = main_routing.DEFAULT_GCELL_LENGTH)
+parser.add_argument('-l','--LAYERS', help='for defining the usable layers', default = main_routing.DEFAULT_LAYERS)
 
 # Parse the arguments
 args = parser.parse_args()
@@ -37,7 +37,7 @@ else:
     print("Error: Invalid placement type. Must be 'rl' or 'rps'")
     exit(1)  # Exit the script with an error code
 
-print("-------------------placement completed----------------")
+print("-------------------Placement completed----------------")
 
 # additional functions
 
